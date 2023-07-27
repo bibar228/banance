e650dccefa44e2c2661158f989d5fff0851013fe
bibar228 <106908322+bibar228@users.noreply.github.com>
2023-07-27 16:11:12 +0500

    final v.1 учитвает изменение цены говнокоина за последние 5 часов учитывает резкие движения битка за последние 5 часов

--- banan/start.py
+++ banan/start.py
@@ -11,22 +11,32 @@ client = Client(keys.api_key, keys.api_secret)
 trading_pairs = ['BCHUSDT', 'XRPUSDT', 'EOSUSDT', 'LTCUSDT', 'TRXUSDT', 'ETCUSDT', 'LINKUSDT', 'XLMUSDT', 'ADAUSDT', 'XMRUSDT', 'DASHUSDT', 'ZECUSDT', 'XTZUSDT', 'BNBUSDT', 'ATOMUSDT', 'ONTUSDT', 'IOTAUSDT', 'BATUSDT', 'VETUSDT', 'NEOUSDT', 'QTUMUSDT', 'IOSTUSDT', 'THETAUSDT', 'ALGOUSDT', 'ZILUSDT', 'KNCUSDT', 'ZRXUSDT', 'COMPUSDT', 'OMGUSDT', 'DOGEUSDT', 'SXPUSDT', 'KAVAUSDT', 'BANDUSDT', 'RLCUSDT', 'WAVESUSDT', 'MKRUSDT', 'SNXUSDT', 'DOTUSDT', 'DEFIUSDT', 'YFIUSDT', 'BALUSDT', 'CRVUSDT', 'TRBUSDT', 'RUNEUSDT', 'SUSHIUSDT', 'SRMUSDT', 'EGLDUSDT', 'SOLUSDT', 'ICXUSDT', 'STORJUSDT', 'BLZUSDT', 'UNIUSDT', 'AVAXUSDT', 'FTMUSDT', 'HNTUSDT', 'ENJUSDT', 'FLMUSDT', 'TOMOUSDT', 'RENUSDT', 'KSMUSDT', 'NEARUSDT', 'AAVEUSDT', 'FILUSDT', 'RSRUSDT', 'LRCUSDT', 'MATICUSDT', 'OCEANUSDT', 'CVCUSDT', 'BELUSDT', 'CTKUSDT', 'AXSUSDT', 'ALPHAUSDT', 'ZENUSDT', 'SKLUSDT', 'GRTUSDT', '1INCHUSDT', 'CHZUSDT', 'SANDUSDT', 'ANKRUSDT', 'BTSUSDT', 'LITUSDT', 'UNFIUSDT', 'REEFUSDT', 'RVNUSDT', 'SFPUSDT', 'XEMUSDT', 'BTCSTUSDT', 'COTIUSDT', 'CHRUSDT', 'MANAUSDT', 'ALICEUSDT', 'HBARUSDT', 'ONEUSDT', 'LINAUSDT', 'STMXUSDT', 'DENTUSDT', 'CELRUSDT', 'HOTUSDT', 'MTLUSDT', 'OGNUSDT', 'NKNUSDT', 'SCUSDT', 'DGBUSDT', '1000SHIBUSDT', 'BAKEUSDT', 'GTCUSDT', 'BTCDOMUSDT', 'IOTXUSDT', 'AUDIOUSDT', 'RAYUSDT', 'C98USDT', 'MASKUSDT', 'ATAUSDT', 'DYDXUSDT', '1000XECUSDT', 'GALAUSDT', 'CELOUSDT', 'ARUSDT', 'KLAYUSDT', 'ARPAUSDT', 'CTSIUSDT', 'LPTUSDT', 'ENSUSDT', 'PEOPLEUSDT', 'ANTUSDT', 'ROSEUSDT', 'DUSKUSDT', 'FLOWUSDT', 'IMXUSDT', 'API3USDT', 'GMTUSDT', 'APEUSDT', 'WOOUSDT', 'FTTUSDT', 'JASMYUSDT', 'DARUSDT', 'GALUSDT', 'OPUSDT', 'INJUSDT', 'STGUSDT', 'FOOTBALLUSDT', 'SPELLUSDT', 'LDOUSDT', 'CVXUSDT', 'ICPUSDT', 'APTUSDT', 'QNTUSDT', 'BLUEBIRDUSDT', 'FETUSDT', 'FXSUSDT', 'HOOKUSDT', 'MAGICUSDT', 'TUSDT', 'RNDRUSDT', 'HIGHUSDT', 'MINAUSDT', 'ASTRUSDT', 'AGIXUSDT', 'PHBUSDT', 'GMXUSDT', 'CFXUSDT', 'STXUSDT', 'COCOSUSDT', 'BNXUSDT', 'ACHUSDT', 'SSVUSDT', 'CKBUSDT', 'PERPUSDT', 'TRUUSDT', 'LQTYUSDT', 'USDCUSDT', 'IDUSDT', 'ARBUSDT', 'JOEUSDT', 'TLMUSDT', 'AMBUSDT', 'LEVERUSDT', 'RDNTUSDT', 'HFTUSDT', 'XVSUSDT', 'BLURUSDT', 'EDUUSDT', 'IDEXUSDT', 'SUIUSDT', 'UMAUSDT', 'RADUSDT', 'KEYUSDT', 'COMBOUSDT', 'NMRUSDT', 'MAVUSDT', 'MDTUSDT', 'XVGUSDT']
 
 
-def top_coin():
+def top_coin(btc_differ):
 
     for i in trading_pairs:
         try:
-            print(i)
-            print(last_data(i, "3m", "300"))
+            #print(i)
+            #print(last_data(i, "3m", "300"))
             data_token = last_data(i, "3m", "300")
-            if data_token[0][-3:] == sorted(data_token[0][-3:]) and sum(data_token[1][:-3])/len(data_token[1][:-3]) * 7 < sum(data_token[1][-3:-1])/3 and 100-(data_token[0][-3]/data_token[0][-1])*100 > 2.4:
+            if data_token[0][-1] > sum(data_token[0][80:-3])/len(data_token[0][80:-3]) \
+                    and data_token[0][-3:] == sorted(data_token[0][-3:]) \
+                    and sum(data_token[1][:-3])/len(data_token[1][:-3]) * 7 < sum(data_token[1][-3:-1])/2 \
+                    and 100-((sum(data_token[0][:-3])/len(data_token[0][:-3]))/data_token[0][-1])*100 > 2.4 \
+                    and btc_differ == True:
                 chat_id = -695765690
                 bot = telebot.TeleBot(telega_token)
                 message = "ALARM - " + i
                 bot.send_message(chat_id, message)
-            if 100-(data_token[0][-3]/data_token[0][-1])*100 > 2.4 and data_token[0][-1] > sum(data_token[0][80:-3])/len(data_token[0][80:-3]):
+            if 100-(data_token[0][-3]/data_token[0][-1])*100 > 2.4 \
+                    and data_token[0][-1] > sum(data_token[0][80:-3])/len(data_token[0][80:-3]) \
+                    and btc_differ == True:
                 chat_id = -695765690
                 bot = telebot.TeleBot(telega_token)
-                message = f"PUMP {i}\n{data_token[0][-3:], data_token[1][-3:]}\nРОСТ ЦЕНЫ НА {round(100-(data_token[0][-3]/data_token[0][-1])*100, 2)}%\nСРЕДНИЙ ОБЪЕМ ТОРГОВ - {int(sum(data_token[1][:-3])/len(data_token[1][:-3]))}"
+                message = f"PUMP {i}\n" \
+                          f"{data_token[0][-3:], data_token[1][-3:]}\n" \
+                          f"РОСТ ЦЕНЫ НА {round(100-(data_token[0][-3]/data_token[0][-1])*100, 2)}%\n" \
+                          f"СРЕДНИЙ ОБЪЕМ ТОРГОВ - {int(sum(data_token[1][:-3])/len(data_token[1][:-3]))}\n" \
+                          f"СРЕДНЯЯ ЦЕНА ЗА ПРОШЛЫЕ 5 ЧАСОВ - {sum(data_token[0][80:-3])/len(data_token[0][80:-3])}"
                 bot.send_message(chat_id, message)
         except:
             pass
@@ -40,7 +50,6 @@ def top_coin():
 #print(top_coin())
 
 def last_data(symbol, interval, lookback):
-
     frame = pd.DataFrame(client.get_historical_klines(symbol, interval, lookback + 'min ago UTC'))
     frame = frame.iloc[:,:6]
     frame.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']
@@ -53,18 +62,25 @@ def last_data(symbol, interval, lookback):
 
 
 def btc_anal(data):
-    if round(sum(data[0][:-1])/28 / data[0][-1] - 1, 3) > 0.28:
+    if round(data[0][-1] / (sum(data[0][:-1])/len(data[0][:-1])) - 1, 3) > 0.24:
         chat_id = -695765690
         bot = telebot.TeleBot(telega_token)
-        message = f"БИТОК РАСТЕТ НА {round(sum(data[0][:-1])/28 / data[0][-1] - 1, 3)}%"
+        message = f"БИТОК РАСТЕТ НА {round((sum(data[0][:-1])/len(data[0][:-1])) / data[0][-1] - 1, 3)}%"
         bot.send_message(chat_id, message)
-
-    print(round((data[0][-1] / (sum(data[0][:-1])/len(data[0][:-1])))-1, 3))
-    print(data[0][-1], sum(data[0][:-1])/len(data[0][:-1]))
-    print(round(data[0][-1] / (sum(data[0][:-1])/len(data[0][:-1]))-1, 1))
-
+        return True
+    elif round(data[0][-1] / (sum(data[0][:-1])/len(data[0][:-1])) - 1, 3) < -0.24:
+        chat_id = -695765690
+        bot = telebot.TeleBot(telega_token)
+        message = f"БИТОК ПАДАЕТ НА {abs(round((sum(data[0][:-1]) / len(data[0][:-1])) / data[0][-1] - 1, 3))}%"
+        bot.send_message(chat_id, message)
+        return False
+    #print(data)
+    #print(sum(data[0][:-1])/len(data[0][:-1]))
+    #print(data[0][-1])
+    return True
 
 
 while True:
-    btc_anal(last_data('BTCUSDT', "5m", "150"))
-    top_coin()
+    btc_differ = btc_anal(last_data('BTCUSDT', "5m", "300"))
+
+    top_coin(btc_differ)
