d2fdafd62afc31cbc4c7718d2d3521f7224a3e5b
bibar228 <profik0077@mail.ru>
2023-09-03 18:53:29 +0500

    web data display

--- web/take_profit/vision/views.py
+++ web/take_profit/vision/views.py
@@ -10,7 +10,10 @@ from vision.serializers import OrdersSerializer
 
 # Create your views here.
 class OrdersViewSet(viewsets.ModelViewSet):
-    queryset = Orders.objects.all()
+    queryset = Orders.objects.filter(side="Купить")[::-1]
+    d = 0
+    for i in queryset:
+        d += float(i.price)
     permission_classes = [
         permissions.AllowAny
     ]
@@ -19,5 +22,10 @@ class OrdersViewSet(viewsets.ModelViewSet):
 
 
 def index(request):
-    orders = Orders.objects.all()
-    return render(request, "vision/index.html", {"orders": orders})
+    orders = Orders.objects.all()[::-1]
+    buys = Orders.objects.filter(side="Купить")[::-1]
+    sells = Orders.objects.filter(side="Продать")[::-1]
+    d = 0
+    for i in range(len(sells)):
+        d += float(sells[i].all_cost) - float(buys[i].all_cost)
+    return render(request, "vision/index.html", {"orders": orders, "buys": round(d, 2)})
