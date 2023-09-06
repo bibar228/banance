7282305eff98959599c90c0fb9f3b7515f4af84e
bibar228 <profik0077@mail.ru>
2023-09-06 21:05:26 +0500

    sql req exceptions

--- banan/sql_request.py
+++ banan/sql_request.py
@@ -1,27 +1,34 @@
 import time
 import pymysql
 from binance.client import Client
+import telebot
 
 api_key = "rxe9zm8PLaF4MT1ZshEcVC0559883oH8LCVTbjOhY5U8TXoDbsbnNfhVBQDZ37xf"
 api_secret = "f41uNN5J8TIeL4lWYCM3n9n4do4e5I1T8YQ5JNiheljyyiKLLAFpEqMSwC3Tsoa3"
 
+telega_token = "5926919919:AAFCHFocMt_pdnlAgDo-13wLe4h_tHO0-GE"
+
 client = Client(api_key, api_secret)
 #i = "SNXUSDT"
-def sql_req(i):
-    orders = client.get_all_orders(symbol=i, limit=1)
-    times = time.localtime(int((str(orders[0]["time"]))[:-3]))
-    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", times)
-    name_cript = orders[0]["symbol"][:-4]
-    if orders[0]["side"] == "SELL":
-        side = "Продать"
-    elif orders[0]["side"] == "BUY":
-        side = "Купить"
-    price = float(orders[0]["price"])
-    count = float(orders[0]["origQty"])
-    all_cost = float(orders[0]['cummulativeQuoteQty'])
-    link_cript = f"https://www.binance.com/ru/trade/{i[:-4]}_USDT?_from=markets&theme=dark&type=grid"
 
-    values = (formatted_time, name_cript, side, price, count, all_cost, link_cript)
+def sql_req(i):
+    try:
+        orders = client.get_all_orders(symbol=i, limit=1)
+        times = time.localtime(int((str(orders[0]["time"]))[:-3]))
+        formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", times)
+        name_cript = orders[0]["symbol"][:-4]
+        if orders[0]["side"] == "SELL":
+            side = "Продать"
+        elif orders[0]["side"] == "BUY":
+            side = "Купить"
+        price = float(orders[0]["price"])
+        count = float(orders[0]["origQty"])
+        all_cost = float(orders[0]['cummulativeQuoteQty'])
+        link_cript = f"https://www.binance.com/ru/trade/{i[:-4]}_USDT?_from=markets&theme=dark&type=grid"
+
+        values = (formatted_time, name_cript, side, price, count, all_cost, link_cript)
+    except Exception as e:
+        telebot.TeleBot(telega_token).send_message(-695765690, f"SQL OSHIBKA: {e}\n")
 
     try:
         connection = pymysql.connect(host='127.0.0.1', port=3306, user='banan_user', password='warlight123',
@@ -35,5 +42,5 @@ def sql_req(i):
         finally:
             connection.close()
 
-    except Exception as ex:
-        print(ex)
+    except Exception as e:
+        telebot.TeleBot(telega_token).send_message(-695765690, f"SQL OSHIBKA: {e}\n")
